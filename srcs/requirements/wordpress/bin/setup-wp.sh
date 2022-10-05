e321d28b07852ad1d3d8295c36a6e9b282dbd9d1
Mathias <mathias.mrsn@gmail.com>
2022-10-05 11:24:36 -0400

    in progress...

--- srcs/requirements/wordpress/bin/setup-wp.sh
+++ srcs/requirements/wordpress/bin/setup-wp.sh
@@ -4,22 +4,28 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /dev/null
 
 # Check if WP-CLI has been downloaded
-&& php wp-cli.phar --info > /dev/null
+php wp-cli.phar --info > /dev/null
 
 # Allow us to execute WP-CLI
-&& chmod +x wp-cli.phar > /dev/null
+chmod +x wp-cli.phar > /dev/null
 
 # Move WP-CLI in local directory
-&& mv wp-cli.phar /usr/local/bin/wp > /dev/null
+mv wp-cli.phar /usr/local/bin/wp > /dev/null
 
 # Move configuration file to the right directory
-&& mv /etc/local/wp-configuration/www.conf /etc/php/7.3/fpm/pool.d/ > /dev/null
+mv /etc/local/wp-configuration/www.conf /etc/php/7.3/fpm/pool.d/ > /dev/null
+mv /etc/local/wp-configuration/wp-config.php /etc/php/7.3/fpm/pool.d/ > /dev/null
 
 # Install wordpress
-&& wp core download --allow-root > /dev/null
+wp core download --allow-root > /dev/null
 
 # Configure WP databases
-&& wp core config --dbname=wordpress --dbuser=utilisateur --dbpass=motdepasse --dbhost=localhost --dbprefix=wp_ > /dev/null
+wp core config --allow-root --dbname=Inception --dbuser=mamaurai --dbpass=Inception123 --dbhost=mariadb:3306 --dbprefix=wp_ > /dev/null
 
 # Add some informations to WP
-&& wp core install --url="votredomaine.com" --title="Titre du Site" --admin_user="nomdutilisateur_admin" --admin_password="motdepasse_administrateur" --admin_email="votre@email.com" > /dev/null
+wp core install --allow-root --url="mamaurai.42.fr" --title="Titre du Site" --admin_user="ben" --admin_password="lebenjos" --admin_email="ben@email.com" > /dev/null
+
+# docker network create net
+# docker run --net=net_test -p 443:443
+# docker volume create -d wordpress_volume
+# docker run -d -v wordpress_volume:/var/www/html 
