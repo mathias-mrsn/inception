dec9fe43b20de3767d8407b240d603e5179aaf32
mathias-mrsn <mathias.mrsn@gmail.com>
2022-10-08 16:44:59 +0200

    `BUG` inception can start but fastcgi does not work

--- srcs/requirements/wordpress/bin/setup-wp.sh
+++ srcs/requirements/wordpress/bin/setup-wp.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
+sleep 100;
+
 # Download WP-CLI tool
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /dev/null
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
 # Check if WP-CLI has been downloaded
-php wp-cli.phar --info > /dev/null
+php wp-cli.phar --info 
 
 # Allow us to execute WP-CLI
-chmod +x wp-cli.phar > /dev/null
+chmod +x wp-cli.phar 
 
 # Move WP-CLI in local directory
-mv wp-cli.phar /usr/local/bin/wp > /dev/null
+mv wp-cli.phar /usr/local/bin/wp 
 
 # Move configuration file to the right directory
-mv /etc/local/wp-configuration/www.conf /etc/php/7.3/fpm/pool.d/ > /dev/null
-# mv /etc/local/wp-configuration/wp-config.php /etc/php/7.3/fpm/pool.d/ > /dev/null
+mv /etc/local/wp-configuration/www.conf /etc/php/7.3/fpm/pool.d/ 
+# mv /etc/local/wp-configuration/wp-config.php /etc/php/7.3/fpm/pool.d/ 
 
 /bin/bash
 # Install wordpress
-wp core download --allow-root > /dev/null
+wp core download --allow-root --path="/var/www/html" 
 
 # Configure WP databases
-wp config create --allow-root --dbname=Inception --dbuser=mamaurai --dbpass=Inception123 --dbhost=mariadb:3306 --dbprefix=wp_ --config-file="/var/www/html/wp-config.php"
+wp config create --allow-root --dbname=Inception --dbuser=mamaurai --dbpass=Inception123 --dbhost=mariadb:3306 --dbprefix=wp_ --config-file="/var/www/html/wp-config.php" --path="/var/www/html"
 
 # Add some informations to WP
-wp core install --allow-root --url="mamaurai.42.fr" --title="Titre du Site" --admin_user="ben" --admin_password="lebenjos" --admin_email="ben@email.com"
+wp core install --allow-root --url="mamaurai.42.fr" --title="Titre du Site" --admin_user="ben" --admin_password="lebenjos" --admin_email="ben@email.com" --path="/var/www/html"
 
+mkdir -p /run/php/
 
-# mkdir -p /run/php/
+php-fpm7.3 -F
 
 # docker network create net
 
