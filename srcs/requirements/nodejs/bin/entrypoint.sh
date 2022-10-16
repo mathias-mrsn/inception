2f75c0f4522481cf977c5f924d47be6b533b6a28
mathias-mrsn <mathias.mrsn@gmail.com>
2022-10-16 18:31:30 +0200

    je ne comprends rien au JS

--- srcs/requirements/nodejs/bin/entrypoint.sh
+++ srcs/requirements/nodejs/bin/entrypoint.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+cd /etc/local/nodejs
+npm -i --save express
+
+# npm install # Install automatically every package asked in package.json
+# npm -i -g ... # Install globally a package
+# npm start
--- srcs/requirements/nodejs/srcs/server/server.js
+++ srcs/requirements/nodejs/srcs/server/server.js
@@ -1,26 +1,138 @@
-let http = require('http'); // Here, you can add variables for each requirement
-let fs = require('fs'); // Here, you can add variables for each requirement
-let url = require('url'); // Here, you can add variables for each requirement
-const EventEmitter = require('events'); // Here, you can add variables for each requirement
+// const express = require('express')
+// const app = express()
 
-let App = {
-    start: function (port) {
-        let emitter = new EventEmitter();
-        let server = http.createServer((req, res) => {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            if (req.url === '/') {
-                emitter.emit('root');
-            }
-            res.end();
-        }).listen(port);
-        return emitter;
-    }
-}
+// // app.get('/', (req, res) => {
+// //     res.send('Hello World!');
+// // })
 
-let app = App.start(8080);
-app.on('root', function (response) {
-    response.write('Je suis root');
+// // app.listen(3000)
+
+// // let app = require('express')();
+
+// app.set('view engine', 'ejs');
+// app.use(express.static('www'));
+
+// app.get('/', (request, response) => {
+//     // response.render('../www/index.html', {test: 'test'});
+//     response.render(pages/index, {test: 'test'});
+// })
+
+// app.listen(3000);
+
+var express = require('express');
+var app = express();
+
+var main_path = '~/42/inception/srcs/requirements/nodejs';
+// app.use(require('morgan')('dev'));
+app.use(express.static(main_path + '/srcs/www'));
+
+
+
+app.get('/', function(req, res) {
+    res.sendFile('~/42/inception/srcs/requirements/nodejs/srcs/www/index.html');
 });
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let http = require('http'); // Here, you can add variables for each requirement
+// let fs = require('fs'); // Here, you can add variables for each requirement
+// let url = require('url'); // Here, you can add variables for each requirement
+// const EventEmitter = require('events'); // Here, you can add variables for each requirement
+
+// let App = {
+//     start: function (port) {
+//         let emitter = new EventEmitter();
+//         let server = http.createServer((req, res) => {
+//             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+//             if (req.url === '/') {
+//                 emitter.emit('root');
+//             }
+//             res.end();
+//         }).listen(port);
+//         return emitter;
+//     }
+// }
+
+// let app = App.start(8080);
+// app.on('root', function (response) {
+//     response.write('Je suis root');
+// });
 
 
 
