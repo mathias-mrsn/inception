8f25d79951a9f27a4b8a1a00be070520ed7b2251
mathias-mrsn <mathias.mrsn@gmail.com>
2022-10-23 15:52:18 +0200

    vsftpd done (BONUS 4/5)

--- srcs/requirements/vsftpd/bin/entrypoint.sh
+++ srcs/requirements/vsftpd/bin/entrypoint.sh
@@ -7,8 +7,12 @@ if [ ! -f ".vsftpd_installed" ]; then
     usermod -g ftp_users mamaurai
     mkdir -p /var/run/vsftpd/empty
     touch .vsftpd_installed
+    echo "vsftpd installed, you can now connect to the ftp server with (mamaurai:Inception123 on port 21)"
 else
     echo "=> vsftpd already installed"
 fi
 
 exec "$@"
+
+# How to test
+# Launch FileZilla with (localhost, mamaurai, Inception123, 21)
