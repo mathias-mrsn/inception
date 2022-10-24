0b231e3c31da5f904810e2c1bab94bacbdb1d6e1
mathias-mrsn <mathias.mrsn@gmail.com>
2022-10-24 22:01:12 +0200

    project done and ready to be pushed

--- srcs/requirements/vsftpd/bin/entrypoint.sh
+++ srcs/requirements/vsftpd/bin/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 if [ ! -f ".vsftpd_installed" ]; then
-    addgroup ftp_users
-    useradd -m mamaurai
-    echo "mamaurai:Inception123" | /usr/sbin/chpasswd
-    usermod -g ftp_users mamaurai
+    addgroup ${FTP_GROUP}
+    useradd -m ${FTP_USER}
+    echo "${FTP_USER}:${FTP_PASSWORD}" | /usr/sbin/chpasswd
+    usermod -g ${FTP_GROUP} ${FTP_USER}
     mkdir -p /var/run/vsftpd/empty
     touch .vsftpd_installed
+    echo "This is a example of text" > /home/${FTP_USER}/example.txt
     echo "vsftpd installed, you can now connect to the ftp server with (mamaurai:Inception123 on port 21)"
 else
     echo "=> vsftpd already installed"
