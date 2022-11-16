f6ac743f93532f05e00304e5cc8c4da202f67cde
mathias-mrsn <mathias.mrsn@gmail.com>
2022-11-16 17:32:21 +0100

    some `ports` keyword changed by `expose`

--- srcs/.dev/clear_all.sh
+++ srcs/.dev/clear_all.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo docker-compose down >/dev/null 2>&1
 docker stop $(docker ps -qa) >/dev/null 2>&1
 docker rmi -f $(docker images -qa) >/dev/null 2>&1
 sudo docker rm $(sudo docker volume ls -q) >/dev/null 2>&1
